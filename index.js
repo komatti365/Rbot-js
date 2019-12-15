5fdfa0cd40654b16f95a494224ffc38ba462fd72
ろんげ･|9 Ø /V (?, € <57322625+ronge-0716@users.noreply.github.com>
2019-12-15 20:27:26 +0900

    「teian」コマンドの追加
      if (command === "teian"){
      const teianMessage = args.join(" ");
      bot.channels.forEach(channel => {
      if (channel.id === "655729271607787522"){
      channel.send({embed:{
      title:"提案が届きました！",
      color: 3066993,
      timestamp: new Date(),
      thumbnail: {
        url: message.guild.iconURL
      },
      fields:[
        {
          name:"提案者",
          value:message.author.tag
        },
        {
          name:"id",
          value:message.author.id
        },
        {
          name:"鯖名",
          value:message.guild.name
        },
        {
          name:"id",
          value:message.guild.id
        },
        {
          name:"提案内容",
          value:teianMessage
        }
      ]
    }});
    }
    });
    }

--- index.js
+++ index.js
@@ -623,6 +623,44 @@ if (command === 'ques'){
     })
   }
 
+  if (command === "teian"){
+  const teianMessage = args.join(" ");
+  bot.channels.forEach(channel => {
+  if (channel.id === "655729271607787522"){
+  channel.send({embed:{
+  title:"提案が届きました！",
+  color: 3066993,
+  timestamp: new Date(),
+  thumbnail: {
+    url: message.guild.iconURL
+  },
+  fields:[
+    {
+      name:"提案者",
+      value:message.author.tag
+    },
+    {
+      name:"id",
+      value:message.author.id
+    },
+    {
+      name:"鯖名",
+      value:message.guild.name
+    },
+    {
+      name:"id",
+      value:message.guild.id
+    },
+    {
+      name:"提案内容",
+      value:teianMessage
+    }
+  ]
+}});
+}
+});
+  }
+
 
   if(command === 'setgame'){
     if(message.author.id !== '502816456052834314', '549881392499130369'){
