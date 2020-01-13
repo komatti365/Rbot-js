90fa2e91892d087a7162db2306e694e0265c4ed0
ろんげ･|9 Ø /V (?, € <57322625+ronge-0716@users.noreply.github.com>
2020-01-13 22:10:24 +0900

    Update index.js

--- index.js
+++ index.js
@@ -812,7 +812,6 @@ bot.on('message', async message => {
     }
     catch (error) {
       console.error(error);
-      message.reply('エラーが発生しました')
       message.channel.send(error.message);
   }else message.channel.send("あなたは開発者ではありません")
   }
