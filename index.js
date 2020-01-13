abbe4d0e8b44740794019511bcbefc42c0bd7a46
ろんげ･|9 Ø /V (?, € <57322625+ronge-0716@users.noreply.github.com>
2020-01-13 22:06:55 +0900

    Update index.js

--- index.js
+++ index.js
@@ -813,13 +813,7 @@ bot.on('message', async message => {
     catch (error) {
       console.error(error);
       message.reply('エラーが発生しました')
-      message.channel.send({
-      embed:{
-      title:"エラー内容",
-      description:error.message
-      }
-      })
-    }
+      message.channel.send(error.message)
   }else message.channel.send("あなたは開発者ではありません")
   }
 });//evalコマンド
